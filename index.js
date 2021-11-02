60be194b89a34b46cf65f0f5ff5a244585fc2b63
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-02 11:28:49 -0300

    inserindo urlendecod

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const route = require('./routes/route');
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+app.use(express.urlencoded({ extended: true}));
 const PORT = process.env.PORT || 3001;
   app.use(cors())
 app.use(route)
