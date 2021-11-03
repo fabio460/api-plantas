444d5e6db67b51d12b08a05e2e9716080263e117
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-03 12:41:35 -0300

    correção na busca do usuario 2.0

--- controllers/controller_auth.js
+++ controllers/controller_auth.js
@@ -2,19 +2,34 @@ const jwt = require('jsonwebtoken');
 const usuario = require('../models/model_usuario')
 
 
-exports.gerar_jwt =async (req,res,next)=>{
+exports.gerar_jwt =async (req,res)=>{
 
-    const queryUsuario = await usuario.find();
-    queryUsuario.find( (item)=>{
-        if(req.body.email === item.email && req.body.senha === item.senha){
-          const token = jwt.sign({usuario:req.body.usuario},'my-secret-key',{expiresIn:300})
-          res.json({usuario:item.nome,token:token});
-          next()
-        }else{
-       
-        }
-    })
-     res.status(401).end()
+  //  const queryUsuario = await usuario.find();
+   // queryUsuario.find( (item)=>{
+     //   if(req.body.email === item.email && req.body.senha === item.senha){
+       //   const token = jwt.sign({usuario:req.body.usuario},'my-secret-key',{expiresIn:300})
+        //  res.json({usuario:item.nome,token:token});
+          
+       // }
+    //})
+    
+   //return res.status(401).send({erro:"não encontrado"})
+    
+   const {email,senha} = req.body;
+   const user =await usuario.findOne({
+     email,
+     senha
+   })
+
+   if(!user){
+     res.status(400).send('usuario não encontrado')
+   }else{
+     return res.json({
+       token : jwt.sign({usuario:req.body.usuario},'my-secret-key',{expiresIn:300}),
+       usuario:user.nome
+     })
+   }
+   
 }
 
 
